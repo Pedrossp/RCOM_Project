687fc68fa028ef5cd5cfdad3ff9767f1a7657054
PP <pedrosspedro@sapo.pt>
2024-10-21 09:12:21 +0100

    Files

---
+++ src/link_layer.c
@@ -0,0 +1,68 @@
+// Link layer protocol implementation
+
+#include "link_layer.h"
+#include "serial_port.h"
+
+// MISC
+#define _POSIX_SOURCE 1 // POSIX compliant source
+
+////////////////////////////////////////////////
+// LLOPEN
+////////////////////////////////////////////////
+int llopen(LinkLayer connectionParameters)
+{
+    if (openSerialPort(connectionParameters.serialPort,
+                       connectionParameters.baudRate) < 0)
+    {
+        return -1;
+    }
+
+    switch (connectionParameters.role)
+    {
+    case LlTx:
+        
+        break;
+        
+    case LlRx:
+
+        break;
+
+    default:
+        break;
+    }
+
+    // TODO
+
+    return 1;
+}
+
+////////////////////////////////////////////////
+// LLWRITE
+////////////////////////////////////////////////
+int llwrite(const unsigned char *buf, int bufSize)
+{
+    // TODO
+
+    return 0;
+}
+
+////////////////////////////////////////////////
+// LLREAD
+////////////////////////////////////////////////
+int llread(unsigned char *packet)
+{
+    // TODO
+
+    return 0;
+}
+
+////////////////////////////////////////////////
+// LLCLOSE
+////////////////////////////////////////////////
+int llclose(int showStatistics)
+{
+    // TODO
+
+    int clstat = closeSerialPort();
+    return clstat;
+}
